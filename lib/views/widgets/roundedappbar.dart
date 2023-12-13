b06a677cbc6afff1d6124a9aad10c3f110d79e96
Kurniia02 <kurniameiarta26@gmail.com>
2023-12-13 09:54:21 +0800

    pembaruan home

---
+++ lib/views/widgets/roundedappbar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class RoundedAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double height;
+  final TabBar tabBar;
+
+  const RoundedAppBar({
+    Key? key,
+    required this.height,
+    required this.tabBar,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        color: Colors.white,
+      ),
+      child: tabBar,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+}
