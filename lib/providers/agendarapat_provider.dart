e835a2a321e2ff9ff654170e5d4f1c8370a5285d
wahyusinggihw <wahyusinggih2402@gmail.com>
2023-12-14 09:01:43 +0800

    search logic and separate appbar from home_screen

--- lib/providers/agendarapat_provider.dart
+++ lib/providers/agendarapat_provider.dart
@@ -10,16 +10,53 @@ class AgendaRapatProvider extends ChangeNotifier {
   List<AgendaRapatSelesaiModel> _agendaRapatSelesaiList = [];
   List<AgendaRapatSelesaiModel> get agendaRapatSelesaiList =>
       _agendaRapatSelesaiList;
+  String searchQuery = '';
+  // searched
+  List<AgendaRapatModel> searchedAgendaRapatList = [];
+  List<AgendaRapatSelesaiModel> searchedAgendaRapatSelesaiList = [];
 
   Future<void> fetchAgendaRapat() async {
     _agendaRapatList = await AgendaRapatService().getAgendaRapat();
-    notifyListeners();
+    // notifyListeners();
+    updateData();
   }
 
   Future<void> fetchAgendaRapatSelesai() async {
     _agendaRapatSelesaiList =
         await AgendaRapatService().getAgendaRapatSelesai();
+    // notifyListeners();
+    updateSelesaiData();
+  }
+
+  // Updated the updateData method to properly filter the list.
+  updateData() {
+    if (searchQuery.isEmpty) {
+      searchedAgendaRapatList = _agendaRapatList;
+    } else {
+      searchedAgendaRapatList = _agendaRapatList
+          .where((element) =>
+              element.agendaRapat.toLowerCase().contains(searchQuery))
+          .toList();
+    }
     notifyListeners();
+  }
+
+  updateSelesaiData() {
+    if (searchQuery.isEmpty) {
+      searchedAgendaRapatSelesaiList = _agendaRapatSelesaiList;
+    } else {
+      searchedAgendaRapatSelesaiList = _agendaRapatSelesaiList
+          .where((element) =>
+              element.agendaRapat.toLowerCase().contains(searchQuery))
+          .toList();
+    }
+    notifyListeners();
+  }
+
+  searchAgendaRapat(String query) {
+    searchQuery = query;
+    updateData();
+    updateSelesaiData();
   }
 
   clearAgendaRapat() {
