70fdf6091c51285d3b656ad9a71b5e8ac86c9032
wahyusinggihw <wahyusinggih2402@gmail.com>
2023-12-18 16:07:52 +0800

    Add SearchHistoryEntry class and update search history functionality

--- lib/providers/search_provider.dart
+++ lib/providers/search_provider.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/foundation.dart';
 
-class SearchHistoryModel extends ChangeNotifier {
-  List<String> searchHistory = [];
+class SearchHistoryEntry {
+  final String title;
+  final String subtitle;
 
-  void addSearchHistory(String query) {
-    if (!searchHistory.contains(query)) {
-      searchHistory.add(query);
+  SearchHistoryEntry({
+    required this.title,
+    required this.subtitle,
+  });
+}
+
+class SearchHistoryModel extends ChangeNotifier {
+  List<SearchHistoryEntry> searchHistory = [];
+
+  void addSearchHistory(String title, String subtitle) {
+    var entry = SearchHistoryEntry(title: title, subtitle: subtitle);
+
+    if (!searchHistory
+        .any((entry) => entry.title == title && entry.subtitle == subtitle)) {
+      searchHistory.add(entry);
+
+      // Keep only the last 3 entries
       if (searchHistory.length > 3) {
         searchHistory.removeAt(0);
       }
+
       notifyListeners();
     }
   }
 
   void removeSearchHistory(int index) {
     searchHistory.removeAt(index);
+    notifyListeners();
+  }
+
+  void clearSearchHistory() {
+    searchHistory.clear();
     notifyListeners();
   }
 }
